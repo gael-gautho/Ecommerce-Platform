36b0fd477e634a507ca084a52fddcad66558aa15
Gael Gautho <gaelgautho07@gmail.com>
2025-08-30 09:05:45 +0200

    Static category list component

--- nextjs_frontend/src/app/page.tsx
+++ nextjs_frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Slider from "@/components/Slider";
 import ProductList from "@/components/ProductList";
+import CategoryList from "@/components/CategoryList";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
           <ProductList/>
+      </div>
+
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+          <CategoryList />
       </div>
 
     </div>
---
+++ nextjs_frontend/src/components/CategoryList.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const cats = [
+  {
+    _id: "1",
+    slug: "t-shirts",
+    name: "T-Shirts",
+    image: { url: "/cat1.jpg" },
+  },
+  {
+    _id: "2",
+    slug: "hoodies",
+    name: "Hoodies",
+    image: { url: "/cat2.jpg" },
+  },
+  {
+    _id: "3",
+    slug: "sneakers",
+    name: "Sneakers",
+    image: { url: "/cat3.jpg" },
+  },
+  {
+    _id: "4",
+    slug: "jeans",
+    name: "Jeans",
+    image: { url: "/cat4.jpg" },
+  },
+  {
+    _id: "5",
+    slug: "accessories",
+    name: "Accessories",
+    image: { url: "/cat5.jpg" },
+  },
+  {
+    _id: "6",
+    slug: "jackets",
+    name: "Jackets",
+    image: { url: "/cat6.jpg" },
+  },
+  
+  {
+    _id: "7",
+    slug: "jackets",
+    name: "Jackets",
+    image: { url: "/cat6.jpg" },
+  },
+];
+
+const CategoryList = async () => {
+
+  return (
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-4 md:gap-8">
+        {cats.map((item) => (
+          <Link
+            href={`/list?cat=${item.slug}`}
+            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            key={item._id}
+          >
+            <div className="relative bg-slate-100 w-full h-96">
+              <Image
+                src={item.image?.url || "cat.png"}
+                alt=""
+                fill
+                sizes="20vw"
+                className="object-cover"
+              />
+            </div>
+            <h1 className="mt-8 font-light text-xl tracking-wide">
+              {item.name}
+            </h1>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
